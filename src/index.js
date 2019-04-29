4c9950bb39ffd6eb3f03757c92b2b669c59207df
Bykov Slava <avel2k6@gmail.com>
2019-04-29 15:59:32 +0300

    add index.js to src

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+const welcome = () => {console.log('Welcome to the Brain Games!'); console.log();};
+const askName = () => readlineSync.question('May I have your name?: ');
+const hello = str => console.log('Hello, ' + str);
+
+export {askName, hello, welcome};
