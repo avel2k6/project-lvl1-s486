608740720e44331aff31b1cdac167cd4bacadb9a
Bykov Slava <avel2k6@gmail.com>
2019-04-30 11:05:49 +0300

    improve code with linter checks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const welcome = () => {console.log('Welcome to the Brain Games!'); console.log();};
+const welcome = () => { console.log('Welcome to the Brain Games!'); console.log(); };
 const askName = () => readlineSync.question('May I have your name?: ');
-const hello = str => console.log('Hello, ' + str);
+const hello = str => console.log(`Hello, ${str}`);
 
-export {askName, hello, welcome};
+export { askName, hello, welcome };
